94d0890fbbc01e114c5e0d0748528566099ad6e8
Max Zhao <mzhao577@gmail.com>
2021-09-11 11:02:02 -0400

    Create ztpl_Job2_SetupEC2_Remotejob.sh

---
+++ ztpl_Job2_SetupEC2_Remotejob.sh
@@ -0,0 +1,17 @@
+#####   Set up EC2.
+machinename=machineindex
+ipaddress=ipaddressValue
+
+ipfile=3ipaddress.txt
+
+###  Step 1.  Change the hostname file.
+sudo cp /etc/hostname /etc/hostname.zkeep
+sudo sed -i "1 s/^.*$/$machinename/" /etc/hostname
+
+###  Step 2.2  Change the hosts file.
+sudo cp /etc/hosts /etc/hosts.zkeep
+sudo sed -i "2 i\127.0.1.1 $machinename"  /etc/hosts
+
+###  Step 2.2  Change the hosts file.
+sudo cat  $ipfile >> /etc/hosts
+
