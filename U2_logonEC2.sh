db29b71f3b5dfaa1eea70622c4a4dc4799856083
Max Zhao <mzhao577@gmail.com>
2021-09-11 11:01:59 -0400

    Create U2_logonEC2.sh

---
+++ U2_logonEC2.sh
@@ -0,0 +1,37 @@
+user=maxzhao
+key0=key0=MaxKeyEast2.pem
+
+keyfile=/Users/$user/aws/securitykey/MaxKeyEast2.pem
+ipfile=/Users/$user/aws/3ipaddress.txt
+
+#######
+if [ $# -eq 1 ]; then
+	loginuser=ubuntu
+	echo "---- mid is: $1"
+	echo "---- Default loginuser is: ubuntu"
+elif [ $# -eq 2 ]; then
+	echo "---- mid is: $1"
+	echo "---- Default loginuser is: $2"
+else
+     echo "Incorrect parameters"
+     echo "$0 [machineindex][loginuser]"
+     exit 2
+
+fi
+
+######
+mid=$1
+loginuser=$2
+echo "The machine name is: machine$mid"
+
+
+####
+#ipadr=$(grep machine${mid} $ipfile | cut -d" " -f1)
+#echo "---- Ip address is: $ipadr"
+
+ipadr=$(grep machine${mid} $ipfile | cut -d" " -f1 | sed 's/\./-/g')
+ec2link="ec2-$ipadr.us-east-2.compute.amazonaws.com"
+
+#ssh -i $keyfile $loginuser@$ipadr
+ssh -i $keyfile $loginuser@$ec2link
+
