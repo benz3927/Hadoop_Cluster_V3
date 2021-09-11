2e1702157dfe2625f13e066841d96099ba565829
Max Zhao <mzhao577@gmail.com>
2021-09-11 11:01:42 -0400

    Create Job1_CreateEC2.sh

---
+++ Job1_CreateEC2.sh
@@ -0,0 +1,26 @@
+
+user=maxzhao
+key0=MaxKeyEast2.pem
+
+templateloc=/Users/$user/aws/createEC2_${user}.cf
+keyfile=/Users/$user/aws/securitykey/$key0
+ipfile=/Users/$user/aws/3ipaddress.txt
+ipfile0=3ipaddress.txt
+
+echo -n "" >  $ipfile0
+
+for idx in  4 5 6 
+#for idx in  8
+do
+	echo "-----  Working on machine $idx"
+	stackname=zstack$idx
+	ec2name=machine$idx	
+	infofile=infokeepFornewEC2_${stackname}_${ec2name}.txt
+	
+	sh U1_createEC2.sh $stackname $ec2name $templateloc
+
+	ipadr=$(cat $infofile)
+	echo "$ipadr machine$idx" >>$ipfile0
+
+done
+
